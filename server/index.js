e22b878e4bf79f798338bf5dadc605ddfa7a8667
theharibalan <srs.haribalan2003@gmail.com>
2024-08-14 21:55:25 +0530

    client

---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./models/Register')
+
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-vercel-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://srsharibalan2003:JWPXHm3ZejPtxAWr@vercel-mern.ade86.mongodb.net/?retryWrites=true&w=majority&appName=vercel-mern');
+    
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        // Your registration logic here
+    } catch (error) {
+        console.error('Registration Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
