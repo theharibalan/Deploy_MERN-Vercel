4f96ebbd084ab86f5b07c8a5d56d956ae2477331
theharibalan <srs.haribalan2003@gmail.com>
2024-08-14 12:26:39 +0530

    server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://deploy-mern-vercel-frontend.vercel.app/"],
+        origin: ["https://deploy-mern-vercel-frontend-mkd2mhd55-hbns-projects-0c5f83d5.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
